fe668defa6e82d0da2797802aac5a42a16e725eb
Ward Childress <ward.childress@gmail.com>
2023-12-18 10:47:39 -0500

    Created a Windows Forms application for .NET 8.0.
    Created a Windows Forms application for .NET 8.0 that currently is an empty form with a couple menu items.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace GBSharp
+{
+	partial class MainForm
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			menuStrip = new MenuStrip();
+			fileToolStripMenuItem = new ToolStripMenuItem();
+			loadROMToolStripMenuItem = new ToolStripMenuItem();
+			exitToolStripMenuItem = new ToolStripMenuItem();
+			helpToolStripMenuItem = new ToolStripMenuItem();
+			aboutGBSharpToolStripMenuItem = new ToolStripMenuItem();
+			menuStrip.SuspendLayout();
+			SuspendLayout();
+			// 
+			// menuStrip
+			// 
+			menuStrip.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, helpToolStripMenuItem });
+			menuStrip.Location = new Point(0, 0);
+			menuStrip.Name = "menuStrip";
+			menuStrip.Size = new Size(800, 24);
+			menuStrip.TabIndex = 0;
+			menuStrip.Text = "menuStrip";
+			// 
+			// fileToolStripMenuItem
+			// 
+			fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { loadROMToolStripMenuItem, exitToolStripMenuItem });
+			fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			fileToolStripMenuItem.Size = new Size(37, 20);
+			fileToolStripMenuItem.Text = "File";
+			// 
+			// loadROMToolStripMenuItem
+			// 
+			loadROMToolStripMenuItem.Name = "loadROMToolStripMenuItem";
+			loadROMToolStripMenuItem.Size = new Size(180, 22);
+			loadROMToolStripMenuItem.Text = "Load ROM...";
+			loadROMToolStripMenuItem.Click += loadROMToolStripMenuItem_Click;
+			// 
+			// exitToolStripMenuItem
+			// 
+			exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			exitToolStripMenuItem.ShortcutKeys = Keys.Alt | Keys.F4;
+			exitToolStripMenuItem.Size = new Size(180, 22);
+			exitToolStripMenuItem.Text = "Exit";
+			exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+			// 
+			// helpToolStripMenuItem
+			// 
+			helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aboutGBSharpToolStripMenuItem });
+			helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			helpToolStripMenuItem.Size = new Size(44, 20);
+			helpToolStripMenuItem.Text = "Help";
+			// 
+			// aboutGBSharpToolStripMenuItem
+			// 
+			aboutGBSharpToolStripMenuItem.Name = "aboutGBSharpToolStripMenuItem";
+			aboutGBSharpToolStripMenuItem.Size = new Size(180, 22);
+			aboutGBSharpToolStripMenuItem.Text = "About GBSharp";
+			aboutGBSharpToolStripMenuItem.Click += aboutGBSharpToolStripMenuItem_Click;
+			// 
+			// MainForm
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(menuStrip);
+			MainMenuStrip = menuStrip;
+			Name = "MainForm";
+			Text = "GBSharp";
+			menuStrip.ResumeLayout(false);
+			menuStrip.PerformLayout();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private MenuStrip menuStrip;
+		private ToolStripMenuItem fileToolStripMenuItem;
+		private ToolStripMenuItem loadROMToolStripMenuItem;
+		private ToolStripMenuItem exitToolStripMenuItem;
+		private ToolStripMenuItem helpToolStripMenuItem;
+		private ToolStripMenuItem aboutGBSharpToolStripMenuItem;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,25 @@
+namespace GBSharp
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void loadROMToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			// TODO: Load ROMs.
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void aboutGBSharpToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			// TODO: Implement about.
+		}
+	}
+}
